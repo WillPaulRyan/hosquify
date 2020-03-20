931d84e8f9fcf4eb7468742b24d663b41e059cfc
Will Ryan <willpaulryan@gmail.com>
2020-03-19 23:12:00 -0400

    Updates

---
+++ precache-manifest.da47e026649771b8640423762b51140e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45180d5fd7b073ccca71163ef082b2d8",
+    "url": "/willpaulryan.github.io/hosquify/index.html"
+  },
+  {
+    "revision": "53b15f082ffa0f96c279",
+    "url": "/willpaulryan.github.io/hosquify/static/css/main.0b262469.chunk.css"
+  },
+  {
+    "revision": "ce71d8f5aae7ea2891db",
+    "url": "/willpaulryan.github.io/hosquify/static/js/2.fe05cde3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/willpaulryan.github.io/hosquify/static/js/2.fe05cde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b15f082ffa0f96c279",
+    "url": "/willpaulryan.github.io/hosquify/static/js/main.73851930.chunk.js"
+  },
+  {
+    "revision": "e8cacbfcddcfd4af9683",
+    "url": "/willpaulryan.github.io/hosquify/static/js/runtime-main.08518516.js"
+  }
+]);
